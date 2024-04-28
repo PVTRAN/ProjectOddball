21d13e3699bf2d97d4d7dcdf08b528957d2c1aad
Jyk0b <jacob.tomassi@uconn.edu>
2024-04-28 19:16:35 -0400

    save

--- Assets/Scripts/FCharacter/MovementsController.cs
+++ Assets/Scripts/FCharacter/MovementsController.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class MovementsController : MonoBehaviour
 {
     public float speed = 5.0f;
+    //---------------------------
     public float jumpForce = 7.0f;
-    
-    //Movement script does not need to be here
-    //public float health; 
+    public float normalJumpForce;
+    //---------------------------
+    public float JumpboostEndTime = 0; // how long boost will be active for 
+    //---------------------------
     private bool isGrounded = true;
-    
+    //---------------------------
     private Rigidbody2D rb;
-    
-    //currently used for sprint that is not working
-    //private float currentSpeed;
-    //public float speedMultiplier = 10.0f;
+    //---------------------------
     
     void Start()
     {
@@ -26,30 +25,40 @@ public class MovementsController : MonoBehaviour
     
     void Update()
     {
-        HandleMovement();
+        HorizontalMovementHandler();
+        BaseJumpHandler();
+        JumpBoostHandler();
+        
     }
     
-    void HandleMovement()
+    void HorizontalMovementHandler()
     {
-        //horizontal movement
         float moveHorizontal = Input.GetAxis("Horizontal") * speed;  
         transform.position += new Vector3(moveHorizontal, 0, 0) * Time.deltaTime;
-            
-        //currently not working :: copied below 
-        /*if (Input.GetKey(KeyCode.LeftShift))
-        {
-            currentSpeed = speed * speedMultiplier;
-        }
-        else
-        {
-            currentSpeed = speed;
-        }*/
-            
+    }
+    
+    void BaseJumpHandler()
+    {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             isGrounded = false;
         }
+    }
+    
+    void JumpBoostHandler()
+    {
+        if (Time.time > JumpboostEndTime) 
+        {
+            jumpForce = normalJumpForce; // jumpForce set back to normal
+        }
+    }
+    public void ActivateJumpBoost(float boost, float duration)
+    {
+        jumpForce = boost; 
+        // when activated, duration will add a boost to Time.time - eventually
+        // Time.time will catch up and set jumpForce back to normalJumpForce as showcase above
+        JumpboostEndTime = Time.time + duration; 
     }
     
     // Detect collision with the ground
@@ -66,23 +75,4 @@ public class MovementsController : MonoBehaviour
 //---------------------------------------
 
 
-// Was moved to a seperate class
-/*void CallDamage()
-        {
-            health = health--;
-        }
-        void CallHealing()
-        {
-            health = health++;
-        }*/
-        
 
-//Not working quite right, uneccessary right now
-/*if (Input.GetKey(KeyCode.LeftShift))
-    {
-        currentSpeed = speed * speedMultiplier;
-    }
-    else
-    {
-        currentSpeed = speed;
-    }*/
